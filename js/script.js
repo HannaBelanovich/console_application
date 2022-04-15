1facb754959d4147f2db7e8780cd9b16ab4a5473
Hanna <Hanna.belanovich@gmail.com>
2022-04-16 00:56:56 +0300

    finished 2d task of 15th lesson

--- js/script.js
+++ js/script.js
@@ -11,13 +11,22 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt ("Один из последних просмотренных фильмов?", ""),
-      b = prompt ("На сколько оцените его?", ""),
-      c = prompt ("Один из последних просмотренных фильмов?", ""),
-      d = prompt ("На сколько оцените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt ("Один из последних просмотренных фильмов?", ""),
+          b = prompt ("На сколько оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+    
+}
 
 console.log(personalMovieDB);
 
